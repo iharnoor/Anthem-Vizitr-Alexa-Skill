9e12d035b7ae07b236a64e313a0813137d3e0f4b
Harnoor Singh <hsingh9@student.gsu.edu>
2017-10-09 16:46:51 -0400

    Anthem Vizitz Skill

---
+++ vizitor_app.py
@@ -0,0 +1,219 @@
+"""
+VERSION 2 CUZ WE MESSED UP
+"""
+
+from __future__ import print_function
+from urllib2 import Request, urlopen, URLError
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Welcome. " \
+                        "Please tell me your phone number by saying, " \
+                        "my phone number is   ."
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Please tell me your phone number by saying, " \
+                        "my phone number is   ."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "App Ended"
+    speech_output = "Thank you for trying VIZITor. " \
+                    "Have a nice day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
+def create_favorite_color_attributes(favorite_color):
+    return {"favoriteColor": favorite_color}
+
+
+def set_color_in_session(intent, session):
+    """ Sets the color in the session and prepares the speech to reply to the
+    user.
+    """
+
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = True
+
+    if 'Number' in intent['slots']:
+        favorite_color = intent['slots']['Number']['value']
+        session_attributes = create_favorite_color_attributes(favorite_color)
+        speech_output = "The number I received is " + \
+                        convert_into_number(favorite_color)
+        #send_number_to_server(favorite_color)
+
+        if checks_for_appointment(favorite_color) is True:
+            speech_output = "I see that you have a scheduled meeting today, let me text you the QR code right now."
+            send_code_to_server(favorite_color)
+        else:
+            speech_output = "I don't see any scheduled meetings for that number, I will send you a text message " \
+                            "with further instructions. "
+            send_number_to_server(favorite_color)
+
+
+        reprompt_text = ""
+    else:
+        speech_output = ""
+        reprompt_text = ""
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+
+def checks_for_appointment(phoneNum):
+    request = Request('http://vizitr.herokuapp.com/checknumber/+1' + str(phoneNum))
+    response = urlopen(request)
+    checkNum = response.read()
+    if checkNum == 'True':
+        return True
+    else:
+        return False
+
+
+
+def convert_into_number(recievedPhoneNumber):
+    StrNum = ""
+    IntNum = ""
+    for i in recievedPhoneNumber:
+            StrNum+= str(i)+" "
+            IntNum+= str(i)
+    return StrNum
+
+def get_color_from_session(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
+        favorite_color = session['attributes']['favoriteColor']
+
+    return favorite_color
+
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "MyColorIsIntent":
+        return set_color_in_session(intent, session)
+    elif intent_name == "WhatsMyColorIntent":
+        return get_color_from_session(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+def send_number_to_server(userInput):
+
+    urlopen(Request('http://vizitr.herokuapp.com/number/' + userInput))
+
+def send_code_to_server(userInput):
+
+    urlopen(Request('http://vizitr.herokuapp.com/sendcode/' + userInput))
